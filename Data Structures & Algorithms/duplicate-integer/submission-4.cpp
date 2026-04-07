acb684c1476beb64b2abffbb261b0ff6a6ca8937
Mooyeong Lee <mylee1995@gmail.com>
2026-04-06 23:15:14 -0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> map;
+        for(int i = 0; i < nums.size(); i++) {
+            if (map.contains(nums[i])) {
+                return true;
+            }
+            map.insert(nums[i]);
+        }
+        return false;
+    }
+};
